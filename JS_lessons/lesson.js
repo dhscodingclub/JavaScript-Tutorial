07f7e147140d071bf82373f13f4dee45b6a9e1d1
azarins1 <az61768@djusdstudents.org>
2020-12-05 22:42:28 -0800

    Bring back lesson.js file, which I accidentally deleted (great commit message, I know -Arden)

---
+++ JS_lessons/lesson.js
@@ -0,0 +1,66 @@
+function refreshLessonPage() {
+    let lessonSection = document.getElementById("lesson");
+    if (lessonNumber == 0) {
+        lessonSection.innerHTML =
+            `<h2><img src="../Logo.png" height=30>Lesson 0: Hello, World!</h2>
+        <p>Whenever you are learining a new programming language, people traditionally make the computer say "Hello World" to ensure that they are able to run a program successfully without complicated code. In JavaScript, a computer can "print" statements to what we call the console using the fsollowing statement:</p>
+        <div class="code">console.log("text");</div>
+        <p>console.log() prints out whatever exists between the parenthesis. If you want to print out text, have quotation marks ("") around the text you want to print. For example:1
+        <div class="code">console.log("Strawberry Jam");</div>
+        <div class="code">--> Strawberry Jam</div>
+        <p>console.log("Strawberry Jam"); prints out "Strawberry Jam" to the console.</p>
+        <p>One final note: every programming statement must end with a semicolon (;). While the JavaScript compiler can usually still run code without semicolons, it can occasionally fail to run the code. Adding semicolons to the end of every statement not only makes your code more reliable, but it also makes your code cleaner.
+        <p><challenge>Challenge: can you code a program that prints "Hello, World!" to the console?</challenge></p>`;
+    } else if (lessonNumber == 1) {
+        lessonSection.innerHTML =
+            `<h2><img src="../Logo.png" height=30>Lesson 1.1: Variables introduction</h2>
+        <p>A variable is a container for storing a data value. Variables are declared with the var keyword, and you can set their data value equal to something by using the equals (=) sign.</p>
+        <div class="code">var x = 5;</div>
+        <p>The line of code above does 2 things. Firstly, it initializes a new variable called x with the "var" keyword. Secondly, it sets the new variable of x to the value of 5.</p>
+        <p>Of course, this value of x is not very helpful if we can't do anything with it. Let's add 6 to x with the following code:</p>
+        <div class="code">var x = 5;</div>
+        <div class="code">x = x + 6;</div>
+        <p>On line 1, we initialize a variable named x, and set it equal to 5. On line 2, set x equal to the sum of x + 6, which in effect adds 6 to x's value. However, we don't have any console output, so we need to ensure that the user see's x's value in the console. So we add a third line to our code:</p>
+        <div class="code">var x = 5;</div>
+        <div class="code">x = x + 6;</div>
+        <div class="code">console.log(x);</div>
+        <div class="code">--> 11</div>
+        <p>In the example above, lines 1-2 do the same things as they did before. However, with line 3, we tell the console to print out the value of x. Notice that there are no quotes; we want to print the value <i>variable</i> x, not "x".</p>
+        <p><challenge>Write some code that has variables x and y. Have x be set equal to 15, and have y be set equal to 8. Then print out to the console what the difference between x and y is (which is x - y).</challenge></p>`;
+    } else if (lessonNumber == 2) {
+        lessonSection.innerHTML =
+            `<h2><img src="../Logo.png" height=30>Lesson 2: Data Types of Number</h2>
+        <p>In the past lesson, we have had our variables be of type Number. Number is a datatype, which is a value type. Number includes integers (whole numbers), and floats (numbers with decimals).</p>
+        <p>As we have already went over, you can use the plus (+) sign to add two numbers together, and you can use the minus (-) sign to subtract one number from another. For multiplication, use the asterisk symbol (*), and for division, use the foreward slash (/) symbol. Just like regular math, the order of operations still apply, and parenthesis () can still be inserted to change the order of operations. Now that you know the corresponding characters for each mathematical operation, you can do more complicated math with numbers.</p>
+        <p><challenge>Write a program that has variable C represent Celsius. Set the the variable representing Celsius equal to 17. Then print out what the Fahrenheit amount would be, according to C. The conversion formula is Fahrenheit = 9/5 * Celsius + 32</challenge></p>`;
+    } else if (lessonNumber == 3) {
+        //&emsp; is a tab
+        lessonSection.innerHTML =
+            `<h2><img src="../Logo.png" height=30>Lesson 3: if/else statements and Number comparisons</h2>
+        <p>So far in our code, we have been running all of our lines of code in order from top to bottom. However, there are cases when you would not want to excecute all of your lines of code, and would like to skip some, based on what is happening in your program. That's where if/else statements come into play. Here is an example:</p>
+        <div class="code">var x = 5;</div>
+        <div class="code">var y = 3;</div>
+        <div class="code">if (x > y){;</div>
+        <div class="code">&emsp; console.log("x is greater than y");</div>
+        <div class="code">} else if (y > x){</div>
+        <div class="code">&emsp; console.log("x is less than y");</div>
+        <div class="code">} else {</div>
+        <div class="code">&emsp; console.log("x is equal to y");</div>
+        <div class="code">}</div>
+        <p>In the above code, if x is greater than y (a larger number than y) on line 3, then we will excecute line 4 in the code, which has the text "x is greater than y" printed to the console. If x <i>is not</i> greater than y, then we continue onward to the first else statement, which appears on line 5; otherwise we will skip all the way to right after line 9 (the end of the if/else statement). On line 5, we test to see if y is greater than x. If true, we will excecute line 6 in the code, which has the text "y is greater than x" printed to the console. If the comparison statement on line 5 turns out to be false, we skip ahead to line 7, which just has an else statement, with no condition. When there is no condition, the else statment is excecuted no matter what, provided that all of the preceeding if/else statements above were false.</p>
+        <p>To check if two values are equal to each other, use <i>double</i> equals signs (==). If you use single equals (=), then the computer will assume you are setting a variable to a value. The extra equal sign is what tells the computer that you are testing if two values are equal.</p>
+        <p><challenge>Write a program that calculates a letter grade based on a score you received. Have a variable called pointsReceived that stores how many points you have scored on the assignment, and have a variable called totalPossiblePoints that stores the highest score that someone could score on the test. Have it be that you scored 59 points out of 73 possible. Remember that an A is anything above 89.9%, a B is anything above 79.9%, a C is anything above 69.9%, a D is anything above 59.9%, and an F is anything lower than a D. Make sure that you test for all cases when testing for your grade, and <strong>have your grade exressed as a decimal, such as 0.50 = 50%</strong>.</challenge></p>`;
+    } else if (lessonNumber == 4) {
+        //&emsp; is a tab
+        lessonSection.innerHTML = `lesson 4`;
+    } else if (lessonNumber == 5) {
+        //&emsp; is a tab
+        lessonSection.innerHTML = `lesson 5`;
+    } else if (lessonNumber == 6) {
+        //&emsp; is a tab
+        lessonSection.innerHTML = `lesson 6`;
+    } else if (lessonNumber == 7) {
+        //&emsp; is a tab
+        lessonSection.innerHTML = `lesson 7`;
+    }
+}
