608046df72e0054552c4a5e8324507cfdead9d1a
Arden Zarins <55220658+azarins1@users.noreply.github.com>
2020-12-20 16:05:43 -0800

    Update Constants.js
    patch up syntax of Constants.js

--- JS_lessons/Constants.js
+++ JS_lessons/Constants.js
@@ -22,10 +22,10 @@ const desiredOutputs =
         ["B"], //3
         ["10", "9.5", "9", "8.5", "8", "7.5", "7", "6.5", "6", "5.5", "5", "4.5", "4", "3.5", "3", "2.5", "2", "1.5", "1", "0.5", "0"], //4
         ["10", "9.5", "9", "8.5", "8", "7.5", "7", "6.5", "6", "5.5", "5", "4.5", "4", "3.5", "3", "2.5", "2", "1.5", "1", "0.5", "0"], //5
-        [true, false] //6
-        ["Batman lives in Gotham City"] //7
-        [], //8
-        [], //9
+        [true, false], //6
+        ["Batman lives in Gotham City"], //7
+        ["insert_answer_here"], //8
+        ["insert_answer_here"], //9
         ["Arden", 11, 3.7] //10
     ];
 const requiredKeywords =
@@ -36,9 +36,9 @@ const requiredKeywords =
         ["var", "/", "pointsReceived", "59", "totalPossiblePoints", "73", "if", "else", ";", ".899", ".799", ".699", ".599", "A", "B", "C", "D", "F", "{", "}"],//3
         ["var", "x", "while"], //4
         ["var", "x", "for", "-="],//5
-        ["function temperatureCheck(", "temperatureCheck(35, 95, 308)", "temperatureCheck(20, 68, 423)", "&&", "return true", "return false", "if (", "else"] //6
+        ["function temperatureCheck(", "temperatureCheck(35, 95, 308)", "temperatureCheck(20, 68, 423)", "&&", "return true", "return false", "if (", "else"], //6
         ["var", "name", "city", ";", " lives in ", "console.log(name", "+"],//7
-        [], //8
-        [],//9
+        ["insert_keywords_here"], //8
+        ["insert_keywords_here"],//9
         ["class", "Student", "constructor", "name", "grade", "gpa", "this.", "graduate", "changeGPA", "rollCall", "var", "Arden", "new", "10", "3.9", "3.7", ".graduate();", ".changeGPA(", ".rollCall();"] //10
     ];
