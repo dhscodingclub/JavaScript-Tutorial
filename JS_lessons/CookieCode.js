7345979875831d6c348d1c0cf5002fa397a94da1
azarins1 <az61768@djusdstudents.org>
2020-12-21 13:33:55 -0800

    add cookie structure (To be worked on)

---
+++ JS_lessons/CookieCode.js
@@ -0,0 +1,34 @@
+var lessonNumber = 0;
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    var wantedLesson = getCookie("lesson");
+    if (wantedLesson != "") {
+        //We have a lesson cookie set, so we go to the cookie lesson saveds
+        lessonNumber = wantedLesson;
+        refreshLessonPage();
+    } else {
+        //Set the lesson equal to 0; they are new and not returning
+        setCookie("lesson", 0, 365);
+        refreshLessonPage();
+    }
+}
